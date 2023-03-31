5b771e8712c98a5ba0f0284982cd7558fae768f2
Imran-A-K <imran.k.professional@gmail.com>
2023-03-31 23:59:04 +0600

    made the blog-cards completely responsive for mobile

--- src/Components/Main/Blog/Blog_Card/Blog_Card.jsx
+++ src/Components/Main/Blog/Blog_Card/Blog_Card.jsx
@@ -16,21 +16,22 @@ const Blog_Card = ({blogData}) => {
       </figure>
       <div className="card-body">
         <div className="flex justify-between items-center">
-          <div className="flex items-center gap-2">
+          <div className="flex max-[600px]:flex-col items-center gap-2">
             <img
-              className=" w-12 h-12 rounded-full"
+              className="-ml-6 w-12 h-12 rounded-full"
               src={author_image}
               alt=""
             />
             <div className="flex flex-col">
-              <p className="font-semibold">{author_name}</p>
-              <p>{publish_date}</p>
+              <p className="max-[600px]:text-xs font-semibold">{author_name}</p>
+              <p className="max-[600px]:text-[10px] max-[600px]:font-semibold">{publish_date}</p>
             </div>
           </div>
 
-          <div className="flex items-center gap-1">
-            <p>{read_time < 10 ? `0${read_time}` : read_time} min read</p>
-<span className="cursor-pointer">
+          <div className="relative flex max-[600px]:flex-col-reverse max-[600px]:gap-5 items-center gap-1">
+            <p className="">{read_time < 10 ? `0${read_time}` : read_time} min read</p>
+{/* <span className="cursor-pointer max-[600px]:absolute max-[600px]:-top-5 max-[600px]:right-0"> */}
+<span className="cursor-pointer max-[600px]:-mr-[60px]">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
 </svg>
