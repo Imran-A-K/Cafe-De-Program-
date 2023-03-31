37dea9e23fb47f9141fe3ea4f47db31f884cbbf0
Imran-A-K <imran.k.professional@gmail.com>
2023-03-31 22:38:08 +0600

    completed blog card structure

--- src/Components/Main/Blog/Blog_Card/Blog_Card.jsx
+++ src/Components/Main/Blog/Blog_Card/Blog_Card.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
+
 const Blog_Card = () => {
+  
   return (
     <div className="card w-full bg-base-100 shadow-xl rounded-b-none">
       <figure className="rounded-b-lg">
@@ -23,11 +25,16 @@ const Blog_Card = () => {
             </div>
           </div>
 
-          <div>
-
+          <div className="flex items-center gap-1">
+            <p>10 min read</p>
+<span className="cursor-pointer">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+</svg>
+</span>
           </div>
         </div>
-        <h2 className="card-title font-bold pt-3 text-3xl">How to utilize Chat-Gpt</h2>
+        <h2 className="card-title font-bold pt-5 pb-3 text-3xl">How to utilize Chat-Gpt</h2>
         
         <div className="card-actions ">
           <p className="flex cursor-pointer underline underline-offset-2 text-[#6047EC] font-semibold">Mark as Read</p>
