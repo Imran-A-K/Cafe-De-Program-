c2696713140dc25c6976c7285ce8970765b71026
Imran-A-K <imran.k.professional@gmail.com>
2023-03-31 23:13:36 +0600

    fetched data from json database  and integrated them with Blog_Card component

--- src/Components/Main/Blog/Blog.jsx
+++ src/Components/Main/Blog/Blog.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Blog_Card from './Blog_Card/Blog_Card'
 
 const Blog = () => {
+  const [blogDb,setBlogDb] = useState([]);
+  useEffect(() =>{
+    fetch('jsonData.json')
+    .then( response => response.json())
+    .then( apiData => setBlogDb(apiData))
+  },[]);
+  
   return (
     <div className='w-full'>
-        <Blog_Card />
+      {
+        blogDb.map( blogData => <Blog_Card key={blogData.id} blogData={blogData} />)
+      }
+        
     </div>
   )
 }
--- src/Components/Main/Blog/Blog_Card/Blog_Card.jsx
+++ src/Components/Main/Blog/Blog_Card/Blog_Card.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 
-const Blog_Card = () => {
+const Blog_Card = ({blogData}) => {
+
+  const { blog_cover_image
+, author_image, author_name, blog_title, publish_date, read_time  } = blogData;
   
   return (
-    <div className="card w-full bg-base-100 shadow-xl rounded-b-none">
-      <figure className="rounded-b-lg">
-        <img
-          src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1206&q=80"
+    <div className=" card w-full bg-base-100 shadow-xl rounded-b-none rounded-t-none ">
+      <figure className="rounded-lg rounded-t-lg pt-1">
+        <img className="rounded-t-lg"
+          src={blog_cover_image}
           alt=""
         />
       </figure>
@@ -16,17 +19,17 @@ const Blog_Card = () => {
           <div className="flex items-center gap-2">
             <img
               className=" w-12 h-12 rounded-full"
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              src={author_image}
               alt=""
             />
             <div className="flex flex-col">
-              <p className="font-semibold">Name Nema</p>
-              <p>Mar 13(4 days ago)</p>
+              <p className="font-semibold">{author_name}</p>
+              <p>{publish_date}</p>
             </div>
           </div>
 
           <div className="flex items-center gap-1">
-            <p>10 min read</p>
+            <p>{read_time < 10 ? `0${read_time}` : read_time} min read</p>
 <span className="cursor-pointer">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
@@ -34,7 +37,7 @@ const Blog_Card = () => {
 </span>
           </div>
         </div>
-        <h2 className="card-title font-bold pt-5 pb-3 text-3xl">How to utilize Chat-Gpt</h2>
+        <h2 className="card-title font-bold pt-5 pb-3 text-3xl">{blog_title}</h2>
         
         <div className="card-actions ">
           <p className="flex cursor-pointer underline underline-offset-2 text-[#6047EC] font-semibold">Mark as Read</p>
