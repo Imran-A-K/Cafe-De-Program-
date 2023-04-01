676f503196695e541e7bf44a1c0aa6e3ab4cacac
Imran-A-K <imran.k.professional@gmail.com>
2023-04-01 14:55:26 +0600

    completed sidebar functionalty

--- src/Components/Main/Sidebar/Blog_Title_Holder/Blog_Title_Holder.jsx
+++ src/Components/Main/Sidebar/Blog_Title_Holder/Blog_Title_Holder.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Blog_Title_Holder = ({blogTitle}) => {
   return (
-    <div className='text-center'>{blogTitle}</div>
+    <div className=' m-2 text-left text-[18px] font-semibold bg-white p-3'>{blogTitle}</div>
   )
 }
 
--- src/Components/Main/Sidebar/Sidebar.jsx
+++ src/Components/Main/Sidebar/Sidebar.jsx
@@ -5,10 +5,10 @@ const Sidebar = ({totalReadTime,bookMarkList}) => {
   return (
     <div className='w-full flex flex-col gap-4 sticky top-0'>
 {/* <input className='bg-teal-300 w-full' type="text" value={totalReadTime} disabled /> */}
-    <p className='text-center pt-3 mt-1 rounded-[4px] bg-[#6047EC]/[0.1] h-12 font-semibold text-[#6047EC]'>Spent time on read: {totalReadTime}</p>
-    <div className='flex flex-col pt-3 gap-4 bg-[#6047EC]/[0.08]'>
-        <p className='text-center  text-2xl font-bold'> Bookmarked Blogs: {bookMarkList.length} </p>
-        <div className='flex flex-col gap-2'>
+    <p className='text-center p-3 mt-1 rounded-[4px] bg-[#6047EC]/[0.1] h-12 text-[20px] font-semibold text-[#6047EC]'>Spent time on read: {totalReadTime}</p>
+    <div className='flex flex-col pt-3 pl-3 pr-5 gap-4 bg-[#6047EC]/[0.08]'>
+        <p className='text-left  text-[20px] font-bold pt-3 pl-3'> Bookmarked Blogs: {bookMarkList.length} </p>
+        <div className='flex flex-col gap-2 pb-3'>
             {
               bookMarkList.map( blog => <Blog_Title_Holder key={blog.id} blogTitle={blog.blog_title} />)
             }
