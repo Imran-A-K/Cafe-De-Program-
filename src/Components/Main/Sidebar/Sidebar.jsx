db2d7306750aba3b8f6478f47de014a3cf848451
Imran-A-K <imran.k.professional@gmail.com>
2023-04-05 12:47:51 +0600

    first commit

--- src/Components/Main/Sidebar/Sidebar.jsx
+++ src/Components/Main/Sidebar/Sidebar.jsx
@@ -3,7 +3,9 @@ import Blog_Title_Holder from './Blog_Title_Holder/Blog_Title_Holder'
 
 const Sidebar = ({totalReadTime,bookMarkList}) => {
   return (
-    <div className='w-full flex flex-col gap-4 '>
+   <div className=''>
+     <div className='w-full flex flex-col gap-4 md:sticky md:top-0'>
+    
     <p className='text-center p-3 mt-1 rounded-[4px] bg-[#6047EC]/[0.1] lg:h-12 text-[20px] font-semibold text-[#6047EC] '>Spent time on read: {totalReadTime} min</p>
     <div className='flex flex-col pt-3 pl-3 pr-5 gap-4 bg-[#6047EC]/[0.08] rounded-[4px]'>
         <p className='text-left  text-[20px] font-bold pt-3 pl-3'> Bookmarked Blogs: {bookMarkList.length} </p>
@@ -13,7 +15,9 @@ const Sidebar = ({totalReadTime,bookMarkList}) => {
             }
         </div>
     </div>
+    
     </div>
+   </div>
   )
 }
 
